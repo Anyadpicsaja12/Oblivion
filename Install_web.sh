0cb157090de1cdabe4b02945b7da49ebf02a31e8
Anyadpicsaja12 <aranyer12@gmail.com>
2025-03-19 06:18:25 +0100

    Create Install_web.sh

---
+++ Install_web.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo '<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oblivion rendszer</title>
+</head>
+<body>
+    <h1>Oblivion rendszer vizuális előnézet</h1>
+    <p>Itt fog megjelenni a teljes funkciólista.</p>
+</body>
+</html>' | sudo tee /var/www/html/index.html
